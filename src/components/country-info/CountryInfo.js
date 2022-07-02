e682ae1d53dea7276cc862fc05b1509ed9a5eed4
MoeShoe <sahlimoussa20@gmail.com>
2022-07-02 13:30:18 +0100

    improve CountryInfo component

--- src/components/country-info/CountryInfo.js
+++ src/components/country-info/CountryInfo.js
@@ -1,7 +1,39 @@
 import styles from "./CountryInfo.module.css";
 
 const CountryInfo = () => {
-  return <h1>Country info card</h1>;
+  return (
+    <>
+      <div className={styles["country-name"]}>Country Name</div>
+      <div className={styles["country-infos-container"]}>
+        <div className={styles["info-column"]}>
+          <div className={styles["info-row"]}>
+            <span>Capital:</span> <span>AYAYA</span>
+          </div>
+
+          <div className={styles["info-row"]}>
+            <span>Language:</span> <span>Weeb</span>
+          </div>
+
+          <div className={styles["info-row"]}>
+            <span>Currency:</span> <span>pats</span>
+          </div>
+        </div>
+        <div className={styles["info-column"]}>
+          <div className={styles["info-row"]}>
+            <span>Population:</span> <span>10_000_000</span>
+          </div>
+
+          <div className={styles["info-row"]}>
+            <span>Size:</span> <span>600_000 Km2</span>
+          </div>
+
+          <div className={styles["info-row"]}>
+            <span>Region:</span> <span>cutie land</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default CountryInfo;
