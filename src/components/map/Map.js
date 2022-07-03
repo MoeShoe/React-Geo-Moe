4c47b4b2d88458c6f3376cb6659bf8bc69a62aa2
MoeShoe <sahlimoussa20@gmail.com>
2022-07-03 13:40:15 +0100

    add working map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
 import styles from "./Map.module.css";
 
 const Map = () => {
-  return <h1>Map card</h1>;
+  useEffect(() => {
+    const map = L.map("map").setView([51.505, -0.09], 13);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution: "© OpenStreetMap",
+    }).addTo(map);
+  }, []);
+
+  return <div id="map" className={styles.mapC}></div>;
 };
 
 export default Map;
