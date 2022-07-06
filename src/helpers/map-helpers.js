fd785143d6a0fbf5be821007a4a443f1f3a743fc
MoeShoe <sahlimoussa20@gmail.com>
2022-07-06 14:26:19 +0100

    fine tune zoom level calc

--- src/helpers/map-helpers.js
+++ src/helpers/map-helpers.js
@@ -1,10 +1,11 @@
-export const calcZoomLevel = ($area) => {
-  if ($area < 150) return 12;
-  if ($area < 1_000) return 11;
-  if ($area < 5_000) return 9;
-  if ($area < 30_000) return 8;
-  if ($area < 200_000) return 7;
-  if ($area < 1_000_000) return 6;
-  if ($area < 5_000_000) return 5;
+export const calcZoomLevel = (area) => {
+  if (area < 150) return 12;
+  if (area < 500) return 11;
+  if (area < 1_000) return 10;
+  if (area < 5_000) return 9;
+  if (area < 30_000) return 8;
+  if (area < 150_000) return 7;
+  if (area < 1_500_000) return 6;
+  if (area < 5_000_000) return 5;
   return 4;
 };
