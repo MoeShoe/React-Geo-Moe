6cce25974a97638222ad68dd77742257137522a1
MoeShoe <sahlimoussa20@gmail.com>
2022-07-14 20:25:05 +0100

    add animations to quizzes page

--- src/pages/QuizzesPage.js
+++ src/pages/QuizzesPage.js
@@ -4,14 +4,60 @@ import Card from "../components/UI/Card";
 const QuizzesPage = () => {
   return (
     <div className={styles["quizzes-container"]}>
-      <Card className={styles["quizz-challenge-select-container"]}>
-        Relaxed
+      <Card className={styles["quiz-challenge-select-container"]}>
+        <div className={styles["quiz-challenge-header"]}>
+          <div>☕</div> Relaxed
+        </div>
+        <ul className={styles["quiz-details"]}>
+          <li className={styles["quiz-detail"]}>
+            <span>🌎</span> Guess the 195 Countries of the world.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌍</span>You have 15 minutes.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌏</span>You have unlimited lives.
+          </li>
+        </ul>
       </Card>
-      <Card className={styles["quizz-challenge-select-container"]}>
-        Challenge
+      <Card className={styles["quiz-challenge-select-container"]}>
+        <div
+          className={styles["quiz-challenge-header"]}
+          style={{ fontWeight: "700", color: "#1e3b8b" }}
+        >
+          <div>🎯</div> Challenge
+        </div>
+        <ul className={styles["quiz-details"]}>
+          <li className={styles["quiz-detail"]}>
+            <span>🌎</span> Guess the 195 Countries of the world.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌍</span>You have 10 minutes.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌏</span>You have 5 lives.
+          </li>
+        </ul>
       </Card>
-      <Card className={styles["quizz-challenge-select-container"]}>
-        HardCore
+      <Card className={styles["quiz-challenge-select-container"]}>
+        <div
+          className={styles["quiz-challenge-header"]}
+          style={{ fontWeight: "900", color: "#ad101f" }}
+        >
+          <div>☠️</div> HardCore
+        </div>
+        <ul className={styles["quiz-details"]}>
+          <li className={styles["quiz-detail"]}>
+            <span>🌎</span> Guess the 252 Countries and Territories of the
+            world.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌍</span>You have 15 minutes.
+          </li>
+          <li className={styles["quiz-detail"]}>
+            <span>🌏</span>You have 3 lives.
+          </li>
+        </ul>
       </Card>
     </div>
   );
