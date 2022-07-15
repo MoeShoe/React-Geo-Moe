99b01210905d1e660a329a5d844130bef9d09f19
MoeShoe <sahlimoussa20@gmail.com>
2022-07-15 16:23:38 +0100

    hydrate quiz component with state

--- src/components/quiz/Quiz.js
+++ src/components/quiz/Quiz.js
@@ -1,21 +1,43 @@
 import { useRef } from "react";
+import { useSelector } from "react-redux";
 
 import styles from "./Quiz.module.css";
 import Card from "../UI/Card";
 
+let isInitial = true;
+
 const Quiz = () => {
   const inputGuess = useRef();
 
+  const { name, numberOfCountries, time, lives, onlyUN } = useSelector(
+    (state) => state.quizzes.quiz.quizParams
+  );
+
+  const formattedLives = Number.isFinite(lives)
+    ? `${lives} Lives`
+    : "Unlimited Lives";
+
+  console.log(new Date(time));
+
+  const formattedTime = new Intl.DateTimeFormat("en-US", {
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(time);
+
   const quizGuessSubmitHandler = (e) => {
     e.preventDefault();
+    inputGuess.current.value = "";
+
+    // undo initial state
+    if (isInitial) isInitial = false;
   };
 
   return (
     <div className={styles["quiz-container"]}>
       <div className={styles["quiz-details-container"]}>
-        <div className={styles["quiz-lives"]}>5 Lives</div>
-        <div className={styles["quiz-type"]}>Relaxed</div>
-        <div className={styles["quiz-time"]}>05:00</div>
+        <div className={styles["quiz-lives"]}>{formattedLives}</div>
+        <div className={styles["quiz-type"]}>{name}</div>
+        <div className={styles["quiz-time"]}>{formattedTime}</div>
       </div>
       <div className={styles["country-cards-container"]}>
         <Card className={styles["country-card"]}>
@@ -45,7 +67,7 @@ const Quiz = () => {
         <input
           className={styles["quiz-input"]}
           maxLength="33"
-          placeholder="Enter your guess here!"
+          placeholder={isInitial ? "Input your guess here!" : ""}
           ref={inputGuess}
         />
       </form>
--- src/constants/QUIZZES_PARAMS.js
+++ src/constants/QUIZZES_PARAMS.js
@@ -1,22 +1,25 @@
 import COUNTRY_NAMES_LIST from "./COUNTRY_NAMES_LIST";
 
 const RELAXED_QUIZ_PARAMS = {
+  name: "Relaxed",
   numberOfCountries: 50,
-  time: 300, // in seconds
+  time: 300 * 1_000, // in milliseconds
   lives: Infinity,
   onlyUN: true, // only include countries that are members or obsersver states in the UN, therefore no territories included.
 };
 
 const CHALLENGE_QUIZ_PARAMS = {
+  name: "Challenge",
   numberOfCountries: COUNTRY_NAMES_LIST.filter((con) => con.isUN).length,
-  time: 600,
+  time: 600 * 1_000,
   lives: 5,
   onlyUN: true,
 };
 
 const HARDCORE_QUIZ_PARAMS = {
+  name: "Hardcore",
   numberOfCountries: COUNTRY_NAMES_LIST.length, // 252 including territories
-  time: 900,
+  time: 900 * 1_000,
   lives: 3,
   onlyUN: false, // non UN members (only countries that are widely recognized to some extent) and territories included
 };
