30139efc5f6cba6113b2b7b82d57f514fdf6f741
MoeShoe <sahlimoussa20@gmail.com>
2022-07-20 03:13:48 +0100

    finish 404 not found page

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
+import styles from "./NotFoundPage.module.css";
+
 //* this is the 404 page
 const NotFoundPage = () => {
-  return <div>404 not found :\</div>;
+  return (
+    <div className={styles["not-found-container"]}>
+      <Link to="/" className={styles["four-zero-four"]}>
+        <span className={styles["four"]}>4</span>
+        <span className={styles["zero"]}>0</span>
+        <span className={styles["four"]}>4</span>
+      </Link>
+      <div className={styles["not-found-description"]}>Page not found.</div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
